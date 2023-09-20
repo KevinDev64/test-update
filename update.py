f625be612d37d78ffcb05cfa37429e714c939332
KevinDev64 <russianbear39157@vk.com>
2023-09-20 22:11:27 +0300

    add support network check

--- update.py
+++ update.py
@@ -1,5 +1,21 @@
 import os
-
+import socket
+REMOTE_SERVER = "github.com"
+def is_connected(hostname):
+  try:
+    # See if we can resolve the host name - tells us if there is
+    # A DNS listening
+    host = socket.gethostbyname(hostname)
+    # Connect to the host - tells us if the host is actually reachable
+    s = socket.create_connection((host, 443), 2)
+    s.close()
+    return True
+  except Exception:
+     pass # We ignore any errors, returning False
+  return False
 input("Ready to update!")
 
-os.system("git fetch && git pull")
+if is_connected(REMOTE_SERVER): os.system("git fetch && git pull")
+else: print("No network!")
+
+input("Done!")
